40143113c41a40c158f49b59c84e9d5653ac94d2
perhapsRay <JamieChristopherRay@gmail.com>
2021-09-24 11:04:31 +0100

    Initial commit

---
+++ RogueProject/RogueProject/Source.cpp
@@ -0,0 +1,158 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+const int LEVELWIDTH = 20;
+const int LEVELHEIGHT = 10;
+
+
+unsigned int playerPositionX = 5;
+unsigned int playerPositionY = 5;
+unsigned int newPlayerPositionX = playerPositionX;
+unsigned int newPlayerPositionY = playerPositionY;
+
+unsigned int health = 20;
+
+char playerChar = '@';
+
+
+char map[LEVELHEIGHT][LEVELWIDTH + 1] =
+{
+"aaaaaaaaaaaaaaaaaaaa",
+"a         a        a",
+"a         a        a",
+"a         a        a",
+"a         a        a",
+"a                  a",
+"a                  a",
+"a             H    a",
+"a                  a",
+"aaaaaaaaaaaaaaaaaaaa"
+};
+
+void gotoScreenPosition(short C, short R)
+{
+	COORD xy;
+	xy.X = C;
+	xy.Y = R;
+	SetConsoleCursorPosition(
+		GetStdHandle(STD_OUTPUT_HANDLE), xy);
+}
+
+void renderMap()
+{
+	for (int i = 0; i < LEVELHEIGHT; i++)
+	{
+		std::cout << map[i] << std::endl;
+	}
+}
+
+void handleInput()
+{
+	//newPlayerPositionX = playerPositionX;
+	//newPlayerPositionY = playerPositionY;
+
+	if (GetKeyState(VK_UP) & 0x8000 && map[playerPositionY - 1][playerPositionX] != 'a')
+	{
+		newPlayerPositionY = playerPositionY - 1;
+	}
+
+	if (GetKeyState(VK_DOWN) & 0x8000 && map[playerPositionY + 1][playerPositionX] != 'a')
+	{
+		newPlayerPositionY = playerPositionY + 1;
+	}
+
+	if (GetKeyState(VK_RIGHT) & 0x8000 && map[playerPositionY][playerPositionX + 1] != 'a')
+	{
+		newPlayerPositionX = playerPositionX + 1;
+	}
+
+	if (GetKeyState(VK_LEFT) & 0x8000 && map[playerPositionY][playerPositionX - 1] != 'a')
+	{
+		newPlayerPositionX = playerPositionX - 1;
+	}
+}
+
+void renderPlayer()
+{
+	// Blank old enemy position
+	gotoScreenPosition(playerPositionX, playerPositionY);
+	std::cout << ' ';
+
+	// Draw new enemy position
+	gotoScreenPosition(newPlayerPositionX, newPlayerPositionY);
+	std::cout << playerChar;
+
+
+
+	Sleep(120);
+}
+
+void renderGUI()
+{
+	gotoScreenPosition(2, LEVELHEIGHT + 3);
+	std::cout << "Health: " << health;
+}
+
+void handleCollisions()
+{
+	char nextMove = map[newPlayerPositionY][newPlayerPositionX];
+
+	int currentPosX = playerPositionX;
+	int currentPosY = playerPositionY;
+
+	switch (nextMove)
+	{
+	case 'a':
+		gotoScreenPosition(2, LEVELHEIGHT + 5);
+		newPlayerPositionX = currentPosX;
+		newPlayerPositionY = currentPosY;
+		std::cout << "Hit Wall";
+		break;
+	case ' ':
+		playerPositionX = newPlayerPositionX;
+		playerPositionY = newPlayerPositionY;
+		break;
+	case 'H':
+		health++;
+		break;
+
+	}
+}
+
+void set_cursor(bool visible) {
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = visible;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+void main()
+{
+
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+
+	MoveWindow(console, r.left, r.top, 800, 800, TRUE);
+
+	renderMap();
+
+	while (true)
+	{
+		// Handles the input and updates the players position
+		handleInput();
+
+		// Render the scene
+		renderPlayer();
+
+		// Render the GUI
+		renderGUI();
+
+		handleCollisions();
+
+		set_cursor(false);
+	}
+
+	system("pause");
+}
